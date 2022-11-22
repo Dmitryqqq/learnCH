aa61cbc570bdaa5932dc5c83506a11cfe354354d
dmitry <shkladmi@gmaile.com>
2022-11-23 00:52:43 +0300

    ex005_MaIIIa

---
+++ example005_IF_Else/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // строчными !!
+{
+Console.WriteLine("Ура, это же Маша! ");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+
+}
+
